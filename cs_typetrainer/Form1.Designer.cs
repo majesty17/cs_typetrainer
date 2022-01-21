213a2eb224acbb5d839ec58a867306f3d79380e0
liuhongwei01 <liuhongwei01@baidu.com>
2022-01-21 17:53:48 +0800

    first commit

---
+++ cs_typetrainer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace cs_typetrainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox_show = new System.Windows.Forms.RichTextBox();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox_show);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox_input);
+            this.splitContainer1.Size = new System.Drawing.Size(940, 662);
+            this.splitContainer1.SplitterDistance = 331;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // richTextBox_show
+            // 
+            this.richTextBox_show.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox_show.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox_show.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox_show.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox_show.Name = "richTextBox_show";
+            this.richTextBox_show.Size = new System.Drawing.Size(940, 331);
+            this.richTextBox_show.TabIndex = 0;
+            this.richTextBox_show.Text = resources.GetString("richTextBox_show.Text");
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_input.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_input.Location = new System.Drawing.Point(0, 0);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(940, 327);
+            this.textBox_input.TabIndex = 1;
+            this.textBox_input.TextChanged += new System.EventHandler(this.textBox_input_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 662);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.RichTextBox richTextBox_show;
+    }
+}
+
---
+++ cs_typetrainer/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cs_typetrainer
+{
+    public partial class Form1 : Form
+    {
+
+        private DateTime st;
+        private Boolean ingame;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ingame = false;
+        }
+        private void textBox_input_TextChanged(object sender, EventArgs e)
+        {
+            string current_text = textBox_input.Text;
+            string model_text = richTextBox_show.Text;
+            //是前缀否
+            if (model_text.IndexOf(current_text) == 0)
+            {
+                Console.Out.WriteLine("sub index !");
+                int len = current_text.Length;
+                //先重置当前颜色
+                richTextBox_show.SelectAll();
+                richTextBox_show.SelectionColor = Color.Black;
+                //已经打印的设置红色
+                richTextBox_show.SelectionStart = 0;
+                richTextBox_show.SelectionLength = len;
+                richTextBox_show.SelectionColor = Color.Red;
+                //从第一次匹配算游戏开始
+                if (ingame == false)
+                {
+                    ingame = true;
+                    st = DateTime.UtcNow;
+                }
+            }
+
+
+            //是否全部完成
+            if (model_text == current_text)
+            {
+                //打印耗时，速度
+                var diff = DateTime.UtcNow - st;//.TotalSeconds;
+                var sec = diff.TotalSeconds + diff.Milliseconds / 1000.0;
+                MessageBox.Show("total sec:        <" + sec +
+                            "> \ntotal characters: <" + model_text.Length +
+                            "> \nspeed:            <" + String.Format("{0:0.00}> c/min",
+                    (model_text.Length / sec * 60)));
+                //恢复颜色
+                richTextBox_show.SelectAll();
+                richTextBox_show.SelectionColor = Color.Black;
+                //置为非游戏状态
+                ingame = false;
+
+            }
+        }
+
+
+    }
+}
